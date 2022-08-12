5cade0ea77fc292084295d98fdf2e95da6b95956
kyleforthman <kyle.forthman@yahoo.com>
2022-08-12 14:06:16 -0600

    feat: Add create-work and create-chapter func

--- backend/src/app/database.clj
+++ backend/src/app/database.clj
@@ -2,7 +2,8 @@
   (:require [clojure.java.io :as io]
             [xtdb.api :as xt]
             [com.stuartsierra.component :as component]
-            [com.stuartsierra.component.repl :refer [set-init]]))
+            [com.stuartsierra.component.repl :refer [set-init]]
+            [clojure.spec.alpha :as s]))
 
 (defrecord Database [dir]
   component/Lifecycle
@@ -29,31 +30,6 @@
 (defn new-database [dir]
   (map->Database {:dir dir}))
 
-(defn full-query
-  [node]
-  (xt/q
-   (xt/db node)
-   '{:find [(pull e [*])]
-     :where [[e :xt/id id]]}))
-
-(defn remove-from-node [node eid]
-  (xt/submit-tx node [[::xt/evict eid]]))
-
-(defn create-user [username]
-  (let [uuid (random-uuid)]
-    (xt/submit-tx node
-                [[::xt/put
-                  {:xt/id uuid
-                   :data-type :user
-                   :user/privilege []
-                   :user/reading-list {}
-                   :user/follows []
-                   :user/sponsors []
-                   :user/username username
-                   :user/reader-preferences {}}]])
-    uuid))
-
-
 ;; TODO move to system.clj
 
 (defn example-system [config-options]
@@ -69,50 +45,110 @@
 (defn stop-dev []
   (alter-var-root #'system component/stop))
 
+;;end TODO
+
+(defn full-query
+  [node]
+  (xt/q
+   (xt/db node)
+   '{:find [(pull e [*])]
+     :where [[e :xt/id id]]}))
+
+(defn remove-from-node [node eid]
+  (xt/submit-tx node [[::xt/evict eid]]))
+
+(defn create-user [username]
+  (let [uuid (random-uuid)]
+    (xt/submit-tx node
+                  [[::xt/put
+                    {:xt/id uuid
+                     :data-type :user
+                     :user/privilege []
+                     :user/reading-list {}
+                     :user/follows []
+                     :user/sponsors []
+                     :user/username username
+                     :user/reader-preferences {}}]])
+    uuid))
+
+(defn filter-name
+  [name]
+  (xt/q (xt/db node)
+        '{:find [uuid]
+          :where [[e :xt/id uuid]
+                  [e :user/username name]]
+          :in [name]}
+        name))
+
+(defn name->uuid [name] (ffirst (filter-name name)))
+
+(defn remove-user [name]
+  (remove-from-node node (name->uuid name)))
+
 (comment
 
   (start-dev)
 
   (def node (get-in system [:db :db]))
 
-
-  (create-user "Leif the Max")
-
   (def working-uuid (create-user "Tazspeare"))
+  (remove-user "Tazspeare")
 
-  (remove-from-node node working-uuid)
+  (defn create-work [title username visibility]
+    (let [uuid (random-uuid)]
+    (xt/submit-tx node
+                  [[::xt/put
+                    {:xt/id uuid
+                     :data-type :work
+                     :work/title title
+                     :work/owner (name->uuid username)
+                     :work/visibility [visibility]
+                     :work/contributors? [] ;; can edit or add chapters, but not delete
+                     :work/blurb? ""
+                     :work/warnings? []
+                     :work/genres? []
+                     :work/tags? []
+                     :work/chapters? {} ;; will practically have at least one chapter
+                     :work/cover? ""
+                     :work/hits? 0}]])
+    uuid))
 
-  (xt/q (xt/db node)
-        '{:find [name user-id]
-          :where [[e :xt/id user-id]
-                  [e :username name]]})
+  (defn create-chapter [title username content]
+    (let [uuid (random-uuid)]
+    (xt/submit-tx node
+                  [[::xt/put
+                    {:xt/id uuid
+                     :data-type :chapter
+                     :chapter/title title
+                     :chapter/content content
+                     :chapter/authors [(name->uuid username)]
+                     :chapter/pre-content? "" ;; or maps with polls?
+                     :chapter/post-content? "" ;; or maps with polls?
+                     :chapter/comments? {}
+                     :chapter/hits? 0
+                     :chapter/early-access? false}]])
+    uuid))
 
+ (create-work "Black Reflections" "Tazspeare" :public)
 
-    (defn filter-name
-    [name]
-    (xt/q (xt/db node)
-          '{:find [uuid]
-            :where [[e :xt/db uuid]
-                    [e :user/username name]]
-            :in [name]}
-            name))
+ (create-chapter "Chapter 1" "Tazspeare" "Leif is the Max-Man")
 
-   (filter-name "Tazspeare")
+ (defn get-work-id-for-user
+   [username]
+   (xt/q (xt/db node)
+        '{:find [uuid]
+          :where [[e :work/owner username]
+                  [e :work/title title]
+                  [e :xt/id uuid]]
+          :in [username]}
+        (name->uuid username)))
 
-  (xt/submit-tx node
-                [[::xt/put
-                  {:data-type :work
-                   :work/title ""
-                   :work/owner account-id
-                   :work/visibility [:public :private :restricted]
-                   :work/contributors? [account-id*] ;; can edit or add chapters, but not delete
-                   :work/blurb? ""
-                   :work/warnings? []
-                   :work/genres? []
-                   :work/tags? []
-                   :work/chapters? {chapter-id index} ;; will practically have at least one chapter
-                   :work/cover? ""
-                   :work/hits? 0}]])
+  (get-work-id-for-user "Tazspeare")
+
+  (defn remove-work [name]
+    (remove-from-node node (ffirst (get-work-id-for-user name))))
+
+  (remove-work "Tazspeare")
 
   (xt/sync node)
 
