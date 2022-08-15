26968aff2fdb532a34654b37493df1e5f79efecc
Davis Finlinson <davisfinlinson@gmail.com>
2022-08-15 11:46:10 -0600

    fix: Change Edit/Reply exclusivity for Comments

--- frontend/src/stories/Comment.js
+++ frontend/src/stories/Comment.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Box, Avatar, Button } from "@mui/material";
+import {
+  Box,
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import ReplyIcon from "@mui/icons-material/Reply";
 
@@ -11,43 +19,77 @@ const Comment = ({
   onReply,
   commentText,
   avatarImage,
+  isOwner,
+  isLocked,
 }) => {
   return (
     <Box
       sx={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "left",
-        typography: "p",
-        mx: "auto",
-        backgroundColor: "background.paper",
-        boxShadow: 2,
-        borderRadius: 3,
+        flexGrow: 1,
+        overflow: "hidden",
+        px: 0.1,
+        /* display: "flex", */
+        /* flexDirection: "row", */
+        /* justifyContent: "left", */
+        /* typography: "p", */
+        /* mx: "auto", */
+        /* backgroundColor: "background.paper", */
+        /* boxShadow: 2, */
+        /* borderRadius: 3, */
       }}
     >
-      <p>{username}</p>
-      <Avatar alt={username} src={avatarImage}></Avatar>
-      <Box
+      <Paper
         sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "left",
-          typography: "p",
+          my: 0.1,
           mx: "auto",
-          backgroundColor: "background.paper",
-          boxShadow: 2,
-          borderRadius: 3,
+          p: 0.1,
+          maxWidth: 800,
         }}
       >
-        <p>{commentText}</p>
-      </Box>
-      <Button variant="outlined" onClick={onEdit} startIcon={<EditIcon />}>
-        Edit
-      </Button>
-      <Button variant="contained" onClick={onReply} endIcon={<ReplyIcon />}>
-        Reply
-      </Button>
-      <p>{timestamp}</p>
+        <Grid container justifyContent="center" alignItems="center" spacing={1}>
+          <Grid item>
+            <Box>
+              <Avatar
+                alt={username}
+                src={avatarImage}
+                sx={{ mx: "auto" }}
+              ></Avatar>
+              <p>{username}</p>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm container flexGrow={1}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "left",
+                typography: "p",
+                mx: "auto",
+                backgroundColor: "background.paper",
+                boxShadow: 2,
+                borderRadius: 3,
+              }}
+            >
+              <p>{commentText}</p>
+            </Box>
+          </Grid>
+          <Grid item>
+            {isOwner ? (
+              <IconButton aria-label="edit" onClick={onEdit}>
+                <EditIcon />
+              </IconButton>
+            ) : null}
+            {isLocked ? null : (
+              <IconButton aria-label="reply" onClick={onReply}>
+                <ReplyIcon />
+              </IconButton>
+            )}
+          </Grid>
+          <Grid item>
+            <p>{timestamp}</p>
+          </Grid>
+        </Grid>
+      </Paper>
     </Box>
   );
 };
--- frontend/src/stories/Comment.stories.js
+++ frontend/src/stories/Comment.stories.js
@@ -12,12 +12,50 @@ export default {
 
 const Template = (args) => <Comment {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const Owner = Template.bind({});
+Owner.args = {
+  isOwner: true,
+  isLocked: false,
   username: "Loganicus Swole",
   timestamp: "8/12/22 5:50 PM",
   commentText:
     "This is a placeholder comment. It does not have any value. The implications of what it says are up to you to decide. Here are more words to make it longer.",
   defaultAvatar: "L",
-  avatarImage: "",
+  avatarImage: "https://avatarfiles.alphacoders.com/594/59437.jpg",
+};
+
+export const OwnerLocked = Template.bind({});
+OwnerLocked.args = {
+  isOwner: true,
+  isLocked: true,
+  username: "Loganicus Swole",
+  timestamp: "8/12/22 5:50 PM",
+  commentText:
+    "This is a placeholder comment. It does not have any value. The implications of what it says are up to you to decide. Here are more words to make it longer.",
+  defaultAvatar: "L",
+  avatarImage: "https://avatarfiles.alphacoders.com/594/59437.jpg",
+};
+
+export const NotOwner = Template.bind({});
+NotOwner.args = {
+  isOwner: false,
+  isLocked: false,
+  username: "Loganicus Swole",
+  timestamp: "8/12/22 5:50 PM",
+  commentText:
+    "This is a placeholder comment. It does not have any value. The implications of what it says are up to you to decide. Here are more words to make it longer.",
+  defaultAvatar: "L",
+  avatarImage: "https://avatarfiles.alphacoders.com/594/59437.jpg",
+};
+
+export const NotOwnerLocked = Template.bind({});
+NotOwnerLocked.args = {
+  isOwner: false,
+  isLocked: true,
+  username: "Loganicus Swole",
+  timestamp: "8/12/22 5:50 PM",
+  commentText:
+    "This is a placeholder comment. It does not have any value. The implications of what it says are up to you to decide. Here are more words to make it longer.",
+  defaultAvatar: "L",
+  avatarImage: "https://avatarfiles.alphacoders.com/594/59437.jpg",
 };
