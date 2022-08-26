a4471bf09773f5bdc3c0da78f60714d1871540d6
loganrios <loganrios1@gmail.com>
2022-08-26 14:40:05 -0600

    fix: clean up test component, default login

--- frontend/src/app/index.cljs
+++ frontend/src/app/index.cljs
@@ -7,21 +7,14 @@
            ["@mui/material" :as mui]
            ["@mui/icons-material" :as mui-icons]
 
-           ["/Testo" :as Testo]))
-
-(def testo (.-default Testo))
+           ["/Login$default" :as Login]))
 
 (defn main []
  [:meta {:name "viewport" :content "initial-scale=1, width=device-width"}]
- [:div
-  [:> mui/Button {:onClick #(js/console.log "Logged in!")
-                  :variant "contained"}
-   "Log in!"]
-  [:> testo]])
-
+ [:> Login {:onLoginSubmit #(js/console.log "Logging in!")
+            :onRegisterNav #(js/console.log "Navigating to Registration...")}])
 
 (defn start []
-  (rf/dispatch-sync [:initialize])
   (rdom/render [main] (js/document.getElementById "app")))
 
 (start)
--- frontend/stories/Testo.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Button } from "@mui/material";
-
-const Testo = () => {
-  return <Button>Testo Button!</Button>;
-};
-
-export default Testo;
