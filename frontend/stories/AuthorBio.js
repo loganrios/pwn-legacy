bfb22544a79f8dd428c25dcb9ff75b02e4518356
Davis Finlinson <davisfinlinson@gmail.com>
2022-08-30 11:38:53 -0600

    fix: Adjust AuthorBio formatting and sponsor modal

--- frontend/stories/AuthorBio.js
+++ frontend/stories/AuthorBio.js
@@ -1,14 +1,22 @@
 import React from "react";
 import {
   Box,
-  Avatar,
-  Paper,
-  Grid,
   Typography,
   Button,
   ButtonBase,
+  Modal,
+  TextField,
+  InputAdornment,
 } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import {
+  styled,
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles";
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 const Img = styled("img")({
   margin: "auto",
@@ -17,88 +25,199 @@ const Img = styled("img")({
   maxHeight: "100%",
 });
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+function SponsorModal({ sponsorText, onSponsorSubmit }) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const [sponsorAmount, setSponsorAmount] = React.useState();
+
+  return (
+    <>
+      <Button variant="contained" aria-label="sponsor" onClick={handleOpen}>
+        Sponsor
+      </Button>
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          sx={{
+            ...style,
+            width: 400,
+            display: "flex",
+            flexDirection: "column",
+            gap: 1,
+          }}
+        >
+          <h2 id="sponsorship-modal">Sponsor</h2>
+          <b1>{sponsorText}</b1>
+          <TextField
+            id="sponsor amount"
+            label="Sponsor Amount"
+            value={sponsorAmount}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">$</InputAdornment>
+              ),
+            }}
+            onChange={(evt) =>
+              setSponsorAmount(evt.target.value.replace(/\D/g, ""))
+            }
+          />
+          <Button
+            variant="contained"
+            onClick={() => {
+              onSponsorSubmit(sponsorAmount), handleClose(handleClose);
+            }}
+          >
+            Submit
+          </Button>
+          <Button variant="outlined" onClick={handleClose}>
+            Cancel
+          </Button>
+        </Box>
+      </Modal>
+    </>
+  );
+}
+
 const AuthorBio = ({
   username,
   bioText,
-  statsText,
+  wordcountText,
+  publicReviewsText,
+  publicRatingsText,
   image,
   isOwner,
   onFollow,
   onSponsor,
+  sponsorText,
+  onSponsorSubmit,
+  onEditAvatar,
 }) => {
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        overflow: "hidden",
-        px: 0.1,
-      }}
-    >
-      <Paper
-        sx={{
-          my: 0.1,
-          mx: "auto",
-          p: 0.1,
-          maxWidth: 800,
-        }}
-      >
-        <Grid container justifyContent="center" alignItems="center" spacing={1}>
-          <Grid item>
-            <Box>
-              <ButtonBase sx={{ width: 128, height: 128 }}>
-                <Img alt="default" src={image} />
-              </ButtonBase>
-              <Box
-                sx={{
-                  flexGrow: 1,
-                  overflow: "hidden",
-                  px: 0.1,
-                  boxShadow: 2,
-                  borderRadius: 3,
-                }}
-              >
-                <b1>{statsText}</b1>
-              </Box>
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm container flexGrow={1}>
-            <h1>{username}</h1>
-            <Box
+    <div>
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            display: "grid",
+            borderRadius: 2,
+            boxShadow: 3,
+            gridTemplateColumns: "auto",
+            gap: 1,
+            p: { xs: 0, sm: 2 },
+            gridTemplateRows: "auto",
+            gridTemplateAreas: {
+              xs: `"avatar avatar username username"
+                   "avatar avatar follow sponsor"
+                   "stats  stats  bio    bio"
+                   "stats  stats  bio    bio"`,
+              sm: `"avatar username username follow sponsor"
+                   "avatar bio      bio      bio    bio"
+                   "stats  bio      bio      bio    bio"`,
+            },
+          }}
+        >
+          <Box
+            sx={{
+              gridArea: "avatar",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "start",
+            }}
+          >
+            <ButtonBase
+              onClick={onEditAvatar}
               sx={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "left",
-                typography: "p",
-                mx: "auto",
-                backgroundColor: "background.paper",
-                boxShadow: 2,
-                borderRadius: 3,
+                width: "auto",
+                height: "auto",
+                maxWidth: { xs: 150, sm: 175 },
+                maxHeight: { xs: 150, sm: 175 },
               }}
             >
-              <p>{bioText}</p>
-            </Box>
-          </Grid>
-          <Grid item>
+              <Img alt="default" src={image} />
+            </ButtonBase>
+          </Box>
+          <Box
+            sx={{
+              gridArea: "username",
+              display: "inline-flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography variant="h4">{username}</Typography>
+          </Box>
+          <Box
+            sx={{
+              gridArea: "follow",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <Button variant="contained" aria-label="follow" onClick={onFollow}>
               Follow
             </Button>
+          </Box>
+          <Box
+            sx={{
+              gridArea: "sponsor",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             {isOwner ? (
               <Button variant="contained" disabled>
                 Sponsor
               </Button>
             ) : (
-              <Button
-                variant="contained"
-                aria-label="sponsor"
-                onClick={onSponsor}
-              >
-                Sponsor
-              </Button>
+              <SponsorModal
+                sponsorText={sponsorText}
+                onSponsorSubmit={onSponsorSubmit}
+              />
             )}
-          </Grid>
-        </Grid>
-      </Paper>
-    </Box>
+          </Box>
+          <Box
+            sx={{
+              gridArea: "bio",
+              display: "flex",
+              alignItems: "flex-start",
+            }}
+          >
+            <Typography variant="b1">{bioText}</Typography>
+          </Box>
+          <Box
+            sx={{
+              gridArea: "stats",
+              display: "flex",
+              flexDirection: "column",
+              lineHeight: 1,
+            }}
+          >
+            <Typography variant="b1">{wordcountText}</Typography>
+            <Typography variant="b1">{publicReviewsText}</Typography>
+            <Typography variant="b1">{publicRatingsText}</Typography>
+          </Box>
+        </Box>
+      </ThemeProvider>
+    </div>
   );
 };
 
--- frontend/stories/AuthorBio.stories.js
+++ frontend/stories/AuthorBio.stories.js
@@ -6,7 +6,8 @@ export default {
   component: AuthorBio,
   argTypes: {
     onFollow: { action: "Follow me into the great unknown" },
-    onSponsor: { action: "Shut up and take my money!" },
+    onSponsorSubmit: { action: "Shut up and take my money!" },
+    onEditAvatar: { action: "Change is inevitable." },
   },
 };
 
@@ -16,7 +17,9 @@ export const Owner = Template.bind({});
 Owner.args = {
   isOwner: true,
   username: "Loganicus Swole",
-  statsText: "Followers: 42, Sponsors: 7, Following, 39",
+  wordcountText: "Wordcount: 69,420",
+  publicReviewsText: "Public Reviews: 69",
+  publicRatingsText: "Public Ratings: 420",
   bioText:
     "My name is Loganicus Swole and I like to read books about weight lifting ship my favorite characters.",
   image: "https://avatarfiles.alphacoders.com/594/59437.jpg",
@@ -26,8 +29,12 @@ export const Visitor = Template.bind({});
 Visitor.args = {
   isOwner: false,
   username: "Loganicus Swole",
-  statsText: "Followers: 42, Sponsors: 7, Following, 39",
+  wordcountText: "Wordcount: 69,420",
+  publicReviewsText: "Public Reviews: 69",
+  publicRatingsText: "Public Ratings: 420",
   bioText:
     "My name is Loganicus Swole and I like to read books about weight lifting ship my favorite characters.",
   image: "https://avatarfiles.alphacoders.com/594/59437.jpg",
+  sponsorText:
+    "Please enter the amount you are paying towards your sponsorship of this author.",
 };
