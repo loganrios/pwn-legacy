e3cccada478dff6dbbd7874edefe622a42da6ced
kyleforthman <kyle.forthman@yahoo.com>
2022-09-09 17:15:09 -0600

    feat: Add WorksList to AuthorProfile Screen

--- frontend/src/app/accounts/events.cljs
+++ frontend/src/app/accounts/events.cljs
@@ -1,7 +1,8 @@
 (ns app.accounts.events
   (:require [re-frame.core :refer [reg-event-db]]
             [app.db :refer [<sub
-                            >evt]]))
+                            >evt]]
+            [app.misc :as misc]))
 
 (reg-event-db
  :accounts/login
@@ -18,17 +19,51 @@
        (assoc :user/email email)
        (assoc :jwt "some random string"))))
 
-;; (reg-event-db
-;;  :user/edit-links
-;;  (fn [db [_ uuid]]))
-
-
-(comment
 (reg-event-db
  :edit-url
  (fn [db [_ user-id label url]]
    (assoc-in db [:users user-id :user/links label] url)))
 
-(>evt [:edit-url :Taz "Discord" "fakediscord.smurf"])
 
- nil)
+(reg-event-db
+ :follow-all-author-works
+ (fn [db [_ user-id author-id]]
+   (assoc-in db [:users user-id :user/following]
+            (set (concat (get-in db [:users user-id :user/following])
+                     (misc/author-works db author-id))))))
+
+(comment
+
+  (let [db app.db/dev-db
+        author-id :Taz
+        user-id :Leif]
+    (assoc-in db [:users user-id :user/following]
+              (set (concat (get-in db [:users user-id :user/following])
+                      (reduce (fn [new-follows work]
+                                (let [work-id (first work)
+                                      work-data (second work)]
+                                  (if (= author-id (:work/owner work-data))
+                                    (conj new-follows work-id)
+                                    new-follows)))
+                              []
+                              (:works db))))))
+
+  (>evt [:follow-all-author-works :Devrey :Taz])
+
+  (<sub [:db])
+
+  (defn author-works [db author-id]
+    (reduce (fn [new-follows work]
+              (let [work-id (first work)
+                    work-data (second work)]
+                (if (= author-id (:work/owner work-data))
+                  (conj new-follows work-id)
+                  new-follows)))
+            []
+            (:works db)))
+
+  (author-works app.db/dev-db :Taz)
+
+  (<sub [:db])
+
+  nil)
--- frontend/src/app/accounts/subs.cljs
+++ frontend/src/app/accounts/subs.cljs
@@ -1,33 +1,65 @@
 (ns app.accounts.subs
   (:require [re-frame.core :refer [reg-sub]]
-            [app.db :refer [<sub]]))
+            [app.db :refer [<sub]]
+            [app.misc :as misc]
+            [app.db :as db]))
 
-(reg-sub :user
-         (fn [db [_ uuid]]
-           (get-in db [:users uuid])))
+(reg-sub
+ :user
+ (fn [db [_ uuid]]
+   (get-in db [:users uuid])))
 
-(reg-sub :links
-           (fn [db [_ uuid]]
-             (map (fn [field]
-                    (let [name (first field)]
-                      (assoc {}
-                             :label name
-                             :url (second field))))
-                  (get-in db [:users uuid :user/links]))))
-
-(defn get-sponsor-amount [user author-id]
- (get-in (second user) [:user/sponsors author-id]))
+(reg-sub
+ :author/format-links
+ (fn [db [_ uuid]]
+   (map (fn [field]
+          (let [name (first field)]
+            (assoc {}
+                   :label name
+                   :url (second field))))
+        (get-in db [:users uuid :user/links]))))
 
 (reg-sub
  :author/sponsor-data
  (fn [db [_ author-id]]
-    (reduce (fn [sponsors user]
-              (if (get-sponsor-amount user author-id)
-                  (conj sponsors {:id (first user)
-                                  :username (:user/username (second user))
-                                  :donation (get-sponsor-amount user author-id)})
-                  sponsors))
-            []
-            (:users db))))
+   (reduce (fn [sponsors user]
+             (let [user-id (first user)
+                   user-data (second user)
+                   sponsor-amount (get-in user-data [:user/sponsoring author-id])]
+               (if sponsor-amount
+                 (conj sponsors {:id user-id
+                                 :username (:user/username user-data)
+                                 :donation sponsor-amount})
+                 sponsors)))
+           []
+           (:users db))))
+
+(reg-sub :author/works-by-status
+         (fn [db [_ author-id status]]
+           (map #(misc/works-list-item %)
+                (map #(misc/id->work db %)
+                     (misc/author-works-by-status db author-id status)))))
+
 (comment
- nil)
+
+  (filter #(= :Taz (:work/owner %)) (vals (:works db/dev-db)))
+
+  (misc/author-works app.db/dev-db :Taz)
+
+  (misc/id->work app.db/dev-db 2)
+
+
+  (<sub [:author/works-by-status :Taz :completed])
+  ;; => ({:value #object[cljs$core$rand_int],
+  ;; :label "World of Broken Dreams",
+  ;; :cover "https://www.royalroadcdn.com/public/covers-large/world-of-broken-dreams-63504.jpg?time=1661125629",
+  ;; :onDashNav #object[Function],
+  ;; :onPageNav #object[Function]}
+  ;; {:value #object[cljs$core$rand_int],
+  ;; :label "Black Reflections",
+  ;; :cover "https://www.royalroadcdn.com/public/covers-large/world-of-broken-dreams-63504.jpg?time=1661125629",
+  ;; :onDashNav #object[Function],
+  ;; :onPageNav #object[Function]})
+
+
+  nil)
