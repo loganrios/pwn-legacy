8898f8da0ad78b6d478e412335123ac1624efe87
loganrios <loganrios1@gmail.com>
2022-11-11 14:26:50 -0700

    feat: traumatically migrate project structure redux

--- backend/env/dev/clj/pwn/app/dev_middleware.clj
+++
@@ -1,5 +0,0 @@
-(ns pwn.app.dev-middleware)
-
-(defn wrap-dev [handler _opts]
-  (-> handler
-      ))
--- backend/test/clj/pwn/app/core_test.clj
+++
@@ -1,8 +0,0 @@
-(ns pwn.app.core-test
-  (:require
-    [pwn.app.test-utils :as utils]
-    [clojure.test :refer :all]))
-
-(deftest example-test
-  (is (= 1 2)))
-
